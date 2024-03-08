431ca194586e3162234edbc3bc703ef3e170b16c
Zlatko Bratkovic <zbratkovic@haproxy.com>
2024-03-08 10:15:57 +0100

    fix bug with unable to use parralel flag

--- options.go
+++ options.go
@@ -25,16 +25,16 @@ func parseTestOptions() (*TestOptions, error) {
 	parser.SetHelp("", "help")
 
 	opts := &TestOptions{
-		Tags:  parser.String("t", "tags", &argparse.Options{Required: true, Help: "build tags"}),
-		Runs:  parser.Int("n", "runs", &argparse.Options{Required: false, Default: 1, Help: "different runs to split tests"}),
-		Shift: parser.Int("s", "shift", &argparse.Options{Required: false, Default: 0, Help: "shift to start with different index"}),
-		Mode:  parser.Selector("m", "mode", []string{"list", "dir", "run", "seq"}, &argparse.Options{Required: false, Default: "run", Help: "list - list tests, dir - list tests with dir, run - normal run]"}),
-		Info:  parser.Flag("", "info", &argparse.Options{Required: false, Default: false, Help: "print commands that are executed"}),
-		// Parallel: parser.Int("p", "parallel", &argparse.Options{Required: false, Help: "number of tests run in parallel"}),
-		Verbose: parser.Flag("v", "verbose", &argparse.Options{Required: false, Help: "verbose mode"}),
-		Race:    parser.Flag("", "race", &argparse.Options{Required: false, Help: "enable data race detection"}),
-		Args:    parser.String("", "args", &argparse.Options{Required: false, Help: "pass command line arguments to test"}),
-		Timeout: parser.String("", "timeout", &argparse.Options{Required: false, Help: "timeout to the test"}),
+		Tags:     parser.String("t", "tags", &argparse.Options{Required: true, Help: "build tags"}),
+		Runs:     parser.Int("n", "runs", &argparse.Options{Required: false, Default: 1, Help: "different runs to split tests"}),
+		Shift:    parser.Int("s", "shift", &argparse.Options{Required: false, Default: 0, Help: "shift to start with different index"}),
+		Mode:     parser.Selector("m", "mode", []string{"list", "dir", "run", "seq"}, &argparse.Options{Required: false, Default: "run", Help: "list - list tests, dir - list tests with dir, run - normal run]"}),
+		Info:     parser.Flag("", "info", &argparse.Options{Required: false, Default: false, Help: "print commands that are executed"}),
+		Parallel: parser.Int("p", "parallel", &argparse.Options{Required: false, Help: "number of tests run in parallel"}),
+		Verbose:  parser.Flag("v", "verbose", &argparse.Options{Required: false, Help: "verbose mode"}),
+		Race:     parser.Flag("", "race", &argparse.Options{Required: false, Help: "enable data race detection"}),
+		Args:     parser.String("", "args", &argparse.Options{Required: false, Help: "pass command line arguments to test"}),
+		Timeout:  parser.String("", "timeout", &argparse.Options{Required: false, Help: "timeout to the test"}),
 	}
 
 	err := parser.Parse(os.Args)
